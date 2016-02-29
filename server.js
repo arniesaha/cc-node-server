804a31b4e51bf29c3db909b677603b01de7d8888
Arnab Saha <arniesaha@gmail.com>
2016-02-29 14:44:57 +0530

    firebase and firebase-queue
    Commit for firebase and firebase-queue

--- server.js
+++ server.js
@@ -17,6 +17,20 @@ server.configure(function(){
     server.use(server.router);
 });
 
+var Queue = require('firebase-queue');
+Firebase = require('firebase');
+var apiKey = 'AIzaSyCcK68Hss_1MuFaYrMar6zJDmiP4om86C8';
+var J
+var gcm = require('node-gcm');
+var queueRef = new Firebase("https://popping-fire-3223.firebaseio.com/queue");
+var queue = new Queue(queueRef, function (data, progress, resolve, reject) {
+    processData(data);
+    console.log(data);
+    setTimeout(function () {
+        resolve();
+    }, 1000)
+
+});
 //setup the errors
 server.error(function(err, req, res, next){
     if (err instanceof NotFound) {
@@ -37,7 +51,6 @@ server.error(function(err, req, res, next){
     }
 });
 server.listen( port);
-
 //Array to Store Package Names from the Client
 var pkgName = {};
 
@@ -73,15 +86,71 @@ io.sockets.on('connection', function(socket){
   });
 });
 
+function updateFollowCount(data, action) {
+    var message = new gcm.Message();
+    var registrationIds = [];
+    switch (action) {
+        case 'add_count' :
+            message.addData('task', 'add_followers_count');
+            break;
+
+        case 'decrease_count' :
+            message.addData('task', 'decrease_followers_count');
+            break;
+    }
+    message.addData('sender_id', data.sender_id);
+    registrationIds.push(data.reciever_id);
+    var sender = new gcm.Sender(apiKey);
+    sender.send(message, registrationIds, 4, function (err, result) {
+        console.log(result);
+    });
+
+}
 
 
+function updateFollowers(data) {
+    var message = new gcm.Message();
+    var registrationIds = [];
+    console.log(data.reciever_ids);
+    var ids = JSON.parse(data.reciever_ids);
+    for( i = 0 ; i <ids.length ;  i++ ){
+        registrationIds.push(ids[i]);
+        console.log(ids[i]);
+    }
+    message.addData('task','followed_update');
+    message.addData('sender_id',data.sender_id);
+    message.addData('changed_field',data.changed_field);
+    message.addData('changed_value',data.changed_value);
+    var sender = new gcm.Sender(apiKey);
+    sender.send(message, registrationIds, 4, function (err, result) {
+        console.log(result);
+    });
+
+}
+
+
+function processData(data) {
+    switch (data.type_task) {
+        case 'add_followers_count' :
+            updateFollowCount(data, "add_count");
+            break;
+        case 'decrease_followers_count' :
+            updateFollowCount(data, "decrease_count");
+            break;
+
+        case 'update_followers' :
+            console.log("We should update the followers");
+            updateFollowers(data);
+            break;
+    }
+
+}
 
 ///////////////////////////////////////////
 //              Routes                   //
 ///////////////////////////////////////////
 
 /////// ADD ALL YOUR ROUTES HERE  /////////
-
 server.get('/', function(req,res){
   res.render('index.jade', {
     locals : { 
@@ -92,18 +161,14 @@ server.get('/', function(req,res){
             }
   });
 });
-
-
 //A Route for Creating a 500 Error (Useful to keep around)
 server.get('/500', function(req, res){
     throw new Error('This is a 500 Error');
 });
-
 //The 404 Route (ALWAYS Keep this as the last route)
 server.get('/*', function(req, res){
     throw new NotFound;
 });
-
 //Get URL parameter
 server.get('/p', function(req, res) {
 	res.send("package name is set to " + req.query.pkgName);
